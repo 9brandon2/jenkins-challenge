ace79fecd3db6e4a41683a38fb083b2a8801888a
bojeda <brandon.ojeda@accenture.com>
2019-12-09 13:19:03 -0600

    Updated README and added pytests

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # jenkins-challenge
 Jenkins challenge assignment
+
+To run pytest:
+    pytest -v tester.py
+
+To run flask app:
+    FLASK_APP=build.py flask run
--- build.py
+++ build.py
@@ -1,31 +1,50 @@
 #!/usr/bin/python3
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
-# from sqlalchemy import create_engine
-# from json import dumps
 
 app = Flask(__name__)
 api = Api(app)
-
 birth_data = {}
 class Birthyear(Resource):
+    def __init__(self, birth_data=birth_data):
+        self.birth_data = birth_data
+
     def get(self, user):
-        return {user: birth_data[user]}
+        print (user)
+        if user in list(self.birth_data.keys()):
+            return {user: self.birth_data[user]}, 200
+        else:
+            return 404
 
     def post(self, user):
-        birth_data[user] = request.form['data']
-        return{user: birth_data[user]}
+        try:
+            data = int(request.form['data'])
+        except ValueError:
+            return 403
+        self.birth_data[user] = data
+        print("length..:", data.bit_length())
+        if data.bit_length() > 32:
+            return "Incorrect Value Size", 400
+        else:
+            print ({user: self.birth_data[user]})
+            return {user: self.birth_data[user]}, 204
 
     def delete(self, user):
-        del birth_data[user]
-        return birth_data
+        if user in list(self.birth_data.keys()):
+            del self.birth_data[user]
+            return self.birth_data, 204
+        else:
+            return 400
         
-class Showall(Resource):
-    def get(self):
-        return birth_data
+# class Showall(Resource):
+#     def __init__(self, birth_data={}):
+#         self.birth_data = birth_data
+
+#     def get(self):
+#         return self.birth_data
 
 api.add_resource(Birthyear, '/<string:user>')
-api.add_resource(Showall, '/')
+# api.add_resource(Showall, '/')
 
 if __name__ == '__main__':
      app.run(debug=True)
